4ba5ce6ebb6d912feba80427392d0a1f526e3609
Jake J <jakejohnson818@gmail.com>
2018-06-19 11:40:03 -0500

    Fixed error I was getting

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -2,7 +2,9 @@ package com.codeup.blog;
 
 import com.codeup.blog.Models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,14 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PostController {
 
     @GetMapping("/posts")
-    public String postsIndex() {
+    public String postsIndex(Model model) {
         return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String postsId() {
+    public String postsId(@PathVariable long id,  Model model) {
+        Post post = new Post("here is my title", "Here is the body");
+        model.addAttribute("post", post);
         return "/posts/show";
     }
+
     @GetMapping("/posts/create")
     @ResponseBody
     public String postsCreate() {
